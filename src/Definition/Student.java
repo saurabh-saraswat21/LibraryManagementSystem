52ce787cb4de585f4d624f9c4c6ffe78eec5b2c4
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-09 22:13:50 +0530

    add constructor and getter method for Student class

--- src/Definition/Student.java
+++ src/Definition/Student.java
@@ -7,4 +7,32 @@ public class Student {
     private MyList<Integer> books = new MyList<Integer>();
     private MyList<String> phoneNumbers = new MyList<>();
 
+    public Student(int rollNo, String firstName, String lastName, MyList<Integer> books, MyList<String> phoneNumbers) {
+        this.rollNo = rollNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public MyList<Integer> getBooks() {
+        return books;
+    }
+
+    public MyList<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
 }
