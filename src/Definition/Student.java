5bb1c91d399e50f844de0d8d8baa3fd787ca85af
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-09 22:07:49 +0530

    add student class

---
+++ src/Definition/Student.java
@@ -0,0 +1,10 @@
+package Definition;
+
+public class Student {
+    private int rollNo;
+    private String firstName;
+    private String lastName;
+    private MyList<Integer> books = new MyList<Integer>();
+    private MyList<String> phoneNumbers = new MyList<>();
+
+}
