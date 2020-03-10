de6c408a613119637f04cc5c6536bdee864661c4
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-10 22:27:31 +0530

    add Main class

---
+++ src/Execution/Main.java
@@ -0,0 +1,13 @@
+package Execution;
+
+import Definition.Library;
+
+public class Main {
+    public static void main(String[] args) {
+        Library lib = new Library();
+        lib.addBook();
+        System.out.println(lib.stock);
+
+
+    }
+}
