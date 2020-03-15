c620fa9ca1fe131eb03242d0f5004f6db4f44f32
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-15 22:20:30 +0530

    add a method getInteger method

--- src/Execution/Main.java
+++ src/Execution/Main.java
@@ -2,6 +2,8 @@ package Execution;
 
 import Definition.Library;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Library lib = new Library();
@@ -11,4 +13,5 @@ public class Main {
         //lib.issueBook();
         lib.discardBook();
     }
+
 }
